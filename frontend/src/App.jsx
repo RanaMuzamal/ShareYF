d872a7111a6e7f8331fa66d7e50553159cf8b451
Rana Muzamal <muzamilrana149@gmail.com>
2023-03-14 21:59:08 +0500

    setup basic routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,25 @@
-import './App.css'
+import {
+  BrowserRouter as Router,
+  Navigate,
+  Routes,
+  Route,
+} from "react-router-dom";
+import Home from "pages/home";
+import Login from "pages/login";
+import Profile from "pages/profile";
 
 function App() {
   return (
-    <div className="App">
-      
+    <div className="app">
+      <Router>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/profile/:id" element={<Profile />} />
+        </Routes>
+      </Router>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
